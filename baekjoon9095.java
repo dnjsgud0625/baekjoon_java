e2b6e9d1406d4099f8fc0b8bd8f317ba5433e72f
dnjsgud0625 <dnjsgud0625@naver.com>
2021-04-23 22:17:49 +0900

    baekjoon9095.java

--- baekjoon9095.java
+++ baekjoon9095.java
@@ -6,17 +6,17 @@ public class baekjoon9095 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] dp = new int [11];
-		dp[1]=1;
-		dp[2]=2;
-		dp[3]=4;
-		
-		for(int i=4;i<11;i++) {
-			dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 4; i < 11; i++) {
+			dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
 		}
-		
-		int t=sc.nextInt();
-		for(int i=0;i<t;i++) {
+
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
 			int n = sc.nextInt();
 			System.out.println(dp[n]);
 		}
