01b793d82f6df27acd1ded5533eb2bf2e036909f
dnjsgud0625 <dnjsgud0625@naver.com>
2021-06-07 21:48:11 +0900

    baekjoon7576.java

--- baekjoon7576.java
+++ baekjoon7576.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class baekjoon7576 {
 	static int n, m;
 	static int[][] box;
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
+	static int[] dx = { 0, -1, 0, 1 };
+	static int[] dy = { -1, 0, 1, 0 };
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
