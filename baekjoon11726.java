5623e48df633d0fd714c39527e1e1690dff61305
dnjsgud0625 <dnjsgud0625@naver.com>
2021-06-10 23:18:35 +0900

    baekjoon11726.java

--- baekjoon11726.java
+++ baekjoon11726.java
@@ -9,14 +9,13 @@ public class baekjoon11726 {
 
 		int n = sc.nextInt();
 
-		int[] dp = new int[n + 1];
+		int[] dp = new int[1001];
 		dp[0] = 0;
 		dp[1] = 1;
 		dp[2] = 2;
-		if (n >= 3) {
-			for (int i = 3; i <= n; i++) {
-				dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-			}
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 		}
 		System.out.println(dp[n]);
 		sc.close();
