fb9d20c5a37dfcb9736b0283bd9c14b00e74154c
dnjsgud0625 <dnjsgud0625@naver.com>
2021-06-11 23:54:10 +0900

    baekjoon2579.java

--- baekjoon2579.java
+++ baekjoon2579.java
@@ -1,24 +1,25 @@
 package baekjoon;
 
-import java.io.*;
+import java.util.*;
 
 public class baekjoon2579 {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		int n = Integer.parseInt(br.readLine());
+		int n = sc.nextInt();
 		int[] arr = new int[n + 1];
 		int[] cache = new int[n + 1];
 		for (int i = 1; i <= n; i++)
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = sc.nextInt();
 
 		cache[1] = arr[1];
-		cache[2] = arr[1] + arr[2];
-		cache[3] = Math.max(arr[1] + arr[3], arr[2] + arr[3]);
-		for (int i = 4; i <= n; i++)
-			cache[i] = Math.max(cache[i - 3] + arr[i] + arr[i - 1], cache[i - 2] + arr[i]);
+		if (n >= 2)
+			cache[2] = arr[1] + arr[2];
+		for (int i = 3; i <= n; i++)
+			cache[i] = Math.max(cache[i - 2], cache[i - 3] + arr[i - 1]) + arr[i];
 
 		System.out.println(cache[n]);
+		sc.close();
 	}
 }
